3634784e933d47af32f947a064391ddac99819a5
nick pomps <nickpomps@MacBook-Pro-6.local>
2021-06-11 16:02:41 -0300

    Final questionnaire

--- src/app/student-questionnaire/student-questionnaire.component.ts
+++ src/app/student-questionnaire/student-questionnaire.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
-import { database } from 'firebase-admin';
-import * as angular from "angular";
 import Project from '../projects/project.model';
 import { ProjectService } from '../service/project.service';
-
-import * as $ from 'jquery';
 import Questionnaire from './student-questionnaire.model';
 @Component({
   selector: 'app-student-questionnaire',
@@ -13,12 +9,97 @@ import Questionnaire from './student-questionnaire.model';
   styleUrls: ['./student-questionnaire.component.css']
 })
 export class StudentQuestionnaireComponent implements OnInit {
-
+  
+  
+  leader: string = ''; 
+  pastCourses: string = ''; 
+  studentRep: string = ''; 
+  ipNDA: string = ''; 
+  pastRemote: string = ''; 
+  acc: string = '';  
+  pastReality: string = ''; 
+  
+  pastAgileSoft: string = ''; 
+  pastGitEX: string = ''; 
+  pastAgileScrum: string = ''; 
+  pastAngular: string = ''; 
+  pastReact: string = ''; 
+  pastNext: string = ''; 
+  pastFlutter: string = ''; 
+  pastFirebase: string = ''; 
+  pastAzure: string = ''; 
+  pastAEM: string = ''; 
+  pastHTMLCSS: string = ''; 
+  pastPHP: string = ''; 
+  pastWire: string = '';
+  pastAnimation: string = '';
   constructor(private db: AngularFireDatabase, private projectService: ProjectService) { }
 
   ngOnInit(): void {
   }
-
+  pastRealityCheck (event: any){
+    this.pastReality = event.target.value;
+  }
+  leaderCheck (event: any){
+    this.leader = event.target.value;
+  }
+  pastCoursesCheck (event: any){
+    this.pastCourses = event.target.value;
+  }
+  studentRepCheck (event: any){
+    this.studentRep = event.target.value;
+  }
+  ipNDACheck (event: any){
+    this.ipNDA = event.target.value;
+  }
+  pastRemoteCheck (event: any){
+    this.pastRemote = event.target.value;
+  }
+  accCheck (event: any){
+    this.acc = event.target.value;
+  }
+  pastAgileSoftCheck (event: any){
+    this.pastAgileSoft = event.target.value;
+  }
+  pastGitEXCheck (event: any){
+    this.pastGitEX = event.target.value;
+  }
+  pastAgileScrumCheck (event: any){
+    this.pastAgileScrum = event.target.value;
+  }
+  pastAngularCheck (event: any){
+    this.pastAngular = event.target.value;
+  }
+  pastReactCheck (event: any){
+    this.pastReact = event.target.value;
+  }
+  pastNextCheck (event: any){
+    this.pastNext = event.target.value;
+  }
+  pastFlutterCheck (event: any){
+    this.pastFlutter = event.target.value;
+  }
+  pastFirebaseCheck (event: any){
+    this.pastFirebase = event.target.value;
+  }
+  pastAzureCheck (event: any){
+    this.pastAzure = event.target.value;
+  }
+  pastAEMCheck (event: any){
+    this.pastAEM = event.target.value;
+  }
+  pastHTMLCSSCheck (event: any){
+    this.pastHTMLCSS = event.target.value;
+  }
+  pastPHPCheck (event: any){
+    this.pastPHP = event.target.value;
+  }
+  pastWireCheck (event: any){
+    this.pastWire = event.target.value;
+  }
+  pastAnimationCheck (event: any){
+    this.pastAnimation = event.target.value;
+  }
 
   makeTeam(Questionnaire: Object){
     this.db.database.ref('StudentQuestionnaire').push(Questionnaire);
@@ -29,6 +110,8 @@ export class StudentQuestionnaireComponent implements OnInit {
     return this.projectService.getProjects();
   }
 
+
+
   addQuest(){
     var familyName = (<HTMLInputElement>document.getElementById("familyName")).value;
     var firstName = (<HTMLInputElement>document.getElementById("firstName")).value;
@@ -37,32 +120,11 @@ export class StudentQuestionnaireComponent implements OnInit {
     var projectChoice1 = (<HTMLInputElement>document.getElementById("projectChoice1")).value;
     var projectChoice2 = (<HTMLInputElement>document.getElementById("projectChoice2")).value;
     var projectChoice3 = (<HTMLInputElement>document.getElementById("projectChoice3")).value;
-    var leader = ($('input[name=leader]:checked').val());
-    var pastCourses = ($('input[name=pastCourses]:checked').val());
-    var studentRep = ($('input[name=studentRep]:checked').val());
-    var ipNDA = ($('input[name=ipNDA]:checked').val());
-    var pastRemote= ($('input[name=pastRemote]:checked').val());
     var improve = (<HTMLInputElement>document.getElementById("improve")).value;
-    var acc = ($('input[name=acc]:checked').val());
-
-    var operatingSystem = (<HTMLInputElement>document.getElementById("operatingSystem")).value;
-    var backgroundEX = (<HTMLInputElement>document.getElementById("backgroundEX")).value;
-    var pastAgileSoft = ($('input[name=pastAgileSoft]:checked').val());
-    var pastGitEX = ($('input[name=pastGitEX]:checked').val());
-    var pastAgileScrum = ($('input[name=pastAgileScrum]:checked').val());
-    var pastAngular = ($('input[name=pastAngular]:checked').val());
-    var pastReact = ($('input[name=pastReact]:checked').val());
-    var pastNext = ($('input[name=pastNext]:checked').val());
-    var pastFlutter = ($('input[name=pastFlutter]:checked').val());
-    var pastFirebase = ($('input[name=pastFirebase]:checked').val());
-    var pastAzure = ($('input[name=pastAzure]:checked').val());
-    var pastAEM = ($('input[name=pastAEM]:checked').val());
-    var pastHTMLCSS = ($('input[name=pastHTMLCSS]:checked').val());
-    var pastPHP = ($('input[name=pastPHP]:checked').val());
-    var pastWire = ($('input[name=pastWire]:checked').val());
-    var pastAnimation = ($('input[name=pastAnimation]:checked').val());
-    var pastReality = ($('input[name=pastReality]:checked').val());
     var timeZone = (<HTMLInputElement>document.getElementById("timeZone")).value;
+    var backgroundEX = (<HTMLInputElement>document.getElementById("backgroundEX")).value;
+    var operatingSystem = (<HTMLInputElement>document.getElementById("operatingSystem")).value;
+
 
     let questionnaire = new Questionnaire();
     questionnaire.familyName = String(familyName);
@@ -72,33 +134,32 @@ export class StudentQuestionnaireComponent implements OnInit {
     questionnaire.projectChoice1 = String(projectChoice1);
     questionnaire.projectChoice2 = String(projectChoice2);
     questionnaire.projectChoice3 = String(projectChoice3);
-    questionnaire.leader = String(leader);
-    questionnaire.pastCourses = String(pastCourses);   
-    questionnaire.ipNDA = String(ipNDA); 
-    questionnaire.pastRemote = String(pastRemote);
+    questionnaire.leader = String(this.leader);
+    questionnaire.pastCourses = String(this.pastCourses);   
+    questionnaire.ipNDA = String(this.ipNDA); 
+    questionnaire.pastRemote = String(this.pastRemote);
     questionnaire.improve = String(improve);
-    questionnaire.studentRep = String(studentRep);
-    questionnaire.acc = String(acc);
+    questionnaire.studentRep = String(this.studentRep);
+    questionnaire.acc = String(this.acc);
 
 
     questionnaire.operatingSystem = String(operatingSystem);
     questionnaire.backgroundEX = String(backgroundEX);
-    questionnaire.pastAgileSoft = String(pastAgileSoft);
-    questionnaire.pastGitEX = String(pastGitEX);
-    questionnaire.pastAgileScrum = String(pastAgileScrum);
-    questionnaire.pastAngular = String(pastAngular);
-    questionnaire.pastReact = String(pastReact);
-    questionnaire.pastNext = String(pastNext);
-    questionnaire.pastFlutter = String(pastFlutter);   
-    questionnaire.pastFirebase = String(pastFirebase); 
-    questionnaire.pastAzure = String(pastAzure);
-    questionnaire.pastAEM = String(pastAEM);
-    questionnaire.pastHTMLCSS = String(pastHTMLCSS);
-    questionnaire.pastPHP = String(pastPHP);
-
-    questionnaire.pastWire = String(pastWire);
-    questionnaire.pastAnimation = String(pastAnimation);   
-    questionnaire.pastReality = String(pastReality); 
+    questionnaire.pastAgileSoft = String(this.pastAgileSoft);
+    questionnaire.pastGitEX = String(this.pastGitEX);
+    questionnaire.pastAgileScrum = String(this.pastAgileScrum);
+    questionnaire.pastAngular = String(this.pastAngular);
+    questionnaire.pastReact = String(this.pastReact);
+    questionnaire.pastNext = String(this.pastNext);
+    questionnaire.pastFlutter = String(this.pastFlutter);   
+    questionnaire.pastFirebase = String(this.pastFirebase); 
+    questionnaire.pastAzure = String(this.pastAzure);
+    questionnaire.pastAEM = String(this.pastAEM);
+    questionnaire.pastHTMLCSS = String(this.pastHTMLCSS);
+    questionnaire.pastPHP = String(this.pastPHP);
+    questionnaire.pastWire = String(this.pastWire);
+    questionnaire.pastAnimation = String(this.pastAnimation);   
+    questionnaire.pastReality = String(this.pastReality); 
     questionnaire.timeZone = String(timeZone); 
 
 
@@ -111,12 +172,11 @@ export class StudentQuestionnaireComponent implements OnInit {
     || questionnaire.pastHTMLCSS == "undefined" || questionnaire.pastWire == "undefined" || questionnaire.pastAnimation == "undefined" 
     || questionnaire.pastReality == "undefined" || questionnaire.timeZone == ""){
     window.alert("Please fill out all required sections");
-    }
-    else{
+   }
+   else{
       this.makeTeam(questionnaire);
       window.alert("Your questionnaire has been submitted");
     }
-    
-    
   }
+
 }
