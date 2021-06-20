4bb3109b9589a837be58455d39e2a3ab34441a3b
StefanKinto <39180652+stefankino@users.noreply.github.com>
2021-06-19 23:21:40 -0300

    add new part for task

--- src/app/client-dash/client-dash.component.ts
+++ src/app/client-dash/client-dash.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import RFP from '../rfp/rfp.model';
 import { RfpService } from '../service/rfp.service';
-
+import { TestimonialService } from '../service/testimonial_client.service';
 import Project from '../projects/project.model';
 import { ProjectService } from '../service/project.service';
-
+import Testimonial from './testimonial.model';
 
 @Component({
   selector: 'app-client-dash',
@@ -12,7 +12,8 @@ import { ProjectService } from '../service/project.service';
   styleUrls: ['./client-dash.component.css']
 })
 export class ClientDashComponent implements OnInit {
-  constructor(private rfpService: RfpService, private projectService: ProjectService) { }
+  constructor(private rfpService: RfpService, private projectService: ProjectService,
+              private testimonial: TestimonialService) { }
 
   ngOnInit(): void {
     if (!localStorage.getItem("isLogin") || !(localStorage.getItem("userType") === "client")) {
@@ -26,5 +27,25 @@ export class ClientDashComponent implements OnInit {
 
   getProjects(): Project[] {
     return this.projectService.getProjects();
+  }
+
+  CreateTestimonial() {
+
+    let newTestimonial = new Testimonial();
+    newTestimonial.client_designation = (<HTMLInputElement>document.getElementById("testimonialTitle")).value;
+    newTestimonial.testimonial_text = (<HTMLInputElement>document.getElementById("testimonialDesc")).value;
+    newTestimonial.rating = (<HTMLInputElement>document.getElementById("testimonialRating")).value;
+    newTestimonial.client_name = localStorage.getItem("name");
+    newTestimonial.company_name = localStorage.getItem("org");
+
+    if(newTestimonial.client_designation == "" ||newTestimonial.testimonial_text  == "" || newTestimonial.rating =="" ){
+      window.alert("Please fill out all sections");
+    }
+    else{
+      this.testimonial.createTestimonial(newTestimonial);
+
+      window.alert("Your testimonial has been created");
+    }
+  
   }
 }
