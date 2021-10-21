cc282f1b544bfcc0e1f2594b0d160c1851965801
Zijian Fan <458644597@qq.com>
2021-10-21 17:49:48 +0800

    Optimization of unsubscribe email function

--- src/app/unsubscribe/unsubscribe.component.ts
+++ src/app/unsubscribe/unsubscribe.component.ts
@@ -10,22 +10,26 @@ export class UnsubscribeComponent implements OnInit {
 
   constructor(private userService: UserService) {
 
-}
+  }
   ngOnInit(): void {
-      throw new Error('Method not implemented.');
+    throw new Error('Method not implemented.');
   }
   testimonials: Object[]
 
 
-  updateEmail(){
+  updateEmail() {
     let email = (<HTMLInputElement>document.getElementById("email")).value;
+    let curEmail = localStorage.getItem('email');
     let user = this.userService.getUserByEmail(email);
-
-    if (user == null){
-      alert ("Invalid input")
-    }
+    if (email == curEmail) {
+      this.userService.updateUser(user, { "emailList": false});
+              alert("Unsubscribe Successfully!")
+              }
+    else if (user == null) {
+      alert("Invalid Input or Unregistered Email Address!")
+      }
     else{
-      this.userService.updateUser(user,{"emailList":false});
+      alert("It is not your current E-mail address! Please try again!")
     }
   }
 }
