19e79e8277d6e5fbd33358ad6731efea9bf7c43f
Arham Arshad <68512793+arhamarshad1@users.noreply.github.com>
2021-11-26 21:02:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const app = express();
- app.use(express.static('dist/X691Website'));
- app.get('/', function (req, res,next) {
-     res.redirect('/');
- app.listen(8080)
+var express = require('express');
+var app = express();
+app.use(express.static('dist/X691Website'));
+app.get('/', function (req, res,next) {
+    res.redirect('/');
+});
+app.listen(8080)
