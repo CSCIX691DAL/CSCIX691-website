5c75d9bba3a03fcd2f4e9f1945d101b983409eae
StefanKinto <39180652+stefankino@users.noreply.github.com>
2021-11-30 02:27:34 -0400

    add dynamic form function
    finally it is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,16 @@ import {Router} from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+ 
 })
 export class AppComponent {
   title = 'X691Website';
+  questions: any[];
 
   constructor(public authService: AuthService, public fireAuth: AngularFireAuth, private router: Router) {
+    
+
 
   }
-
-
-
 }
--- src/app/dynamic-form/dynamic-form-question.component.ts
+++ src/app/dynamic-form/dynamic-form-question.component.ts
@@ -14,8 +14,3 @@ export class DynamicFormQuestionComponent {
 }
 
 
-/*
-Copyright 2017-2018 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
