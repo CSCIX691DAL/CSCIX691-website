3a2566700a96501d55e151e74e735296ac774204
ibhatia <is998912@dal.ca>
2022-04-01 17:34:48 -0300

    Past project page

--- src/app/past/past.component.ts
+++ src/app/past/past.component.ts
@@ -12,8 +12,10 @@ export class PastComponent implements OnInit {
   hide=[];
 
   showDiv = {
-    fall: true,
-    summer: true,
+    fall: false,
+    summer: false,
+    winter1: false,
+    winter2: false,
   };
 
   constructor(private projectsService: ProjectService) { }
@@ -30,4 +32,58 @@ export class PastComponent implements OnInit {
     });
     return projects;
   }
+
+  getValue(val:string){
+
+    var count =0;
+
+    const searchRes = [];
+
+    let projects = this.projectsService.getProjects();
+
+    projects.filter((project, index, array) => {
+
+      if( 
+        
+        (project.term ==='Summer 2021' || project.term ==='Winter 2021' || project.term ==='Fall 2021'|| project.term ==='Winter 2022' ) &&
+      ( (project.title.toLowerCase() ).includes(val.toLowerCase() ) || (project.client.toLowerCase() ).includes(val.toLowerCase() ) || 
+      (project.descLong.toLowerCase() ).includes(val.toLowerCase() ) )
+       )
+       
+       {
+
+        searchRes.push(project);
+
+        document.getElementById("outerSearch").style.cssText = "padding: 30px; width: 79%; margin: 10px auto";
+        document.getElementById("innerSearch").style.cssText = "padding: 30px; background-color: #707372; border: solid 1px #707372; color: white;";
+
+        document.getElementById("printVal").innerHTML = project.client;
+        document.getElementById("printVal2").innerHTML = project.descShort;
+        document.getElementById("printVal3").innerHTML = project.descLong;
+        document.getElementById("printVal4").innerHTML = project.teamLeader;
+        document.getElementById("printVal0").innerHTML = project.title;
+
+      }
+    });
+
+    console.log(searchRes);
+
+    if(val === ''){
+      document.getElementById("outerSearch").style.cssText = "";
+        document.getElementById("innerSearch").style.cssText = "";
+        document.getElementById("printVal").innerHTML = '';
+        document.getElementById("printVal2").innerHTML = '';
+        document.getElementById("printVal3").innerHTML = '';
+        document.getElementById("printVal4").innerHTML = '';
+        document.getElementById("printVal0").innerHTML = '';
+
+        count = 0;
+
+        searchRes.length = 0;
+    }
+
+
+  }
+
+
 }
