eda308178d8d481dc03f17371e4e0da95e71a1a2
hanson <hn427433@dal.ca>
2022-06-13 14:19:26 -0300

    ldap-build backend node server: create function and new packages required for the ldap thing

--- node_src/Controllers/authController.js
+++ node_src/Controllers/authController.js
@@ -1,7 +1,27 @@
+const ldap = require('ldapjs');
 const { response } = require("express");
-
+const { authenticate } = require('ldap-authentication')
 
 exports.testAuth = (request, response) => {
+  console.log("testAuth been called")
 
-  response.send("hey you just made a call to auth router testAuth! ᕕ( ᐛ )ᕗ")
+  authenticated = ldapAuthentication("some", "thing");
+
+  console.log(authenticated);
+
+  response.send("hey you just made a call to auth router testAuth! ᕕ( ᐛ )ᕗ");
+}
+
+
+async function ldapAuthentication(username, password) {
+
+  let authenticated = await authenticate({
+    ldapOpts: { url: 'ldap://fcsldap.cs.dal.ca:389' },
+    userDn: `uid=${username},cn=users,dc=cs,dc=dal,dc=ca`,
+    userPassword: password,
+    userSearchBase: 'cn=users,dc=cs,dc=dal,dc=ca',
+    usernameAttribute: 'uid',
+    username: `${username}`,
+  })
+  return authenticated;
 }
