eddc4c03916abd5cafae17884e6bbefc0c8eb3d1
hanson <hn427433@dal.ca>
2022-06-26 19:07:40 -0300

    ldap-build backend node server: disable the session thing for now

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ var express = require('express');
 var server = express();
 
 //session
-const session = require('express-session');
+// const session = require('express-session');
 
 const api = require("./node_src/Routers/index");
 const {response} = require("express");
 
-//60min
-server.use(session({
-  name: 'identityKey',
-  secret: 'secretKey',
-  saveUninitialized: false,
-  resave: false,
-  cookie: {
-    maxAge: 60 * 60 * 1000
-  }
-}));
+// //60min
+// server.use(session({
+//   name: 'identityKey',
+//   secret: 'secretKey',
+//   saveUninitialized: false,
+//   resave: false,
+//   cookie: {
+//     maxAge: 60 * 60 * 1000
+//   }
+// }));
 
 server.use(express.json());
 
