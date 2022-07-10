424a70ec3659c7b6c037f844480c795732b8c92b
Wgeorge13 <64384814+Wgeorge13@users.noreply.github.com>
2022-07-10 14:07:21 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
 module.exports = {
-  plugins: ["react"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  rules: {
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error"
-  }
+  extends: ["eslint:recommended"]
 };
