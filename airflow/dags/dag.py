95ade1da58fe3e9306ee1adcce111e61d6139d3b
shahdk33 <93958185+shahdk33@users.noreply.github.com>
2022-07-25 18:00:59 -0300

    Airflow test (#126)
    * initial airflow code with sample DAG
    
    * Airflow initial code
    
    * Airflow branch python operator fixed
    
    * Final grades operator updated
    
    * airflow code with api
    
    * airflow code with API
    
    * Delete airflow/dags/__pycache__ directory
    
    delete pycache
    
    Co-authored-by: LHansoon <76853769+LHansoon@users.noreply.github.com>
    Co-authored-by: hanson <hn427433@dal.ca>

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -5,7 +5,21 @@ from datetime import datetime
 from random import randint
 from airflow.operators.dummy import DummyOperator
 
+import json
+from datetime import datetime
+from airflow.models import DAG
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+
 from tenacity import retry
+
+def save_students(ti):
+    #xcom pull the return values from the get_posts tasks
+    students = ti.xcom_pull(task_ids=['get_grades'])
+    #specify the path we want to write to
+    with open('\opt\airflow\data\posts.json', 'w') as f:
+        json.dump(students[0], f)
 
 #Defining the functions for the tasks
 def hire():
@@ -27,17 +41,16 @@ def assignTAs():
     return 'Assign TAs to projects'
 
 
-#Tasks that have a "grade" > random int between 0-100 
+#Tasks that have a "grade" > random int between 0-100
 def _training_model(ti):
-    mark = randint(0, 100)*0.01
+    mark = randint(0, 100)*0.05
     ti.xcom_push(key='grade', value=mark)
-    
+
 
 def _planning_phase(ti):
-    mark = randint(0, 100)*0.9
+    mark = randint(0, 100)*0.05
     ti.xcom_push(key='grade', value=mark)
 
-    
 
 def quizzes(ti):
     mark = randint(0,100)*0.05
@@ -52,25 +65,25 @@ def _iterations(ti):
     ti.xcom_push(key='grade', value=mark)
 
 def closing(ti):
-    mark = randint(0,100)*0.3
+    mark = randint(0,100)*0.30
     ti.xcom_push(key='grade', value=mark)
 
 def _final_grades(ti):
     mark = ti.xcom_pull(key='grade', task_ids=['Project_Plan', 'Team_Charter', 'NDA', 'planning', 'student_Q', 'quizzes', '_iterations', 'closing_document'])
-    
-    
+
+
     #choose pass or fail
     finalgrade = sum(mark)
     if finalgrade>60:
         return 'pass'
     else :
-        return 'fail'  
+        return 'fail'
 
 
 #DAG code
 with DAG("CSCIX691_process",
-start_date=datetime(2021, 6 ,14), 
-schedule_interval='@daily', 
+start_date=datetime(2021, 7 ,2),
+schedule_interval='@daily',
 catchup=False) as dag:
 
     training_model_tasks = [
@@ -83,37 +96,134 @@ catchup=False) as dag:
         ) for model_id in ['Project_Plan', 'Team_Charter', 'NDA']
     ]
 
+    task_is_api_active= HttpSensor(
+        task_id='is_api_active',
+        #establishing the HTTP connection
+        http_conn_id = 'api_users',
+        endpoint = 'users/'
+    )
+
+
+#Getting the response (and convert to json objects)
+    task_get_users= SimpleHttpOperator(
+        task_id='get_grades',
+        http_conn_id='api_users',
+        endpoint='users/',
+        method='GET',
+        #tells the task to get the text of the api response and convert it to json
+        response_filter=lambda response: json.loads(response.text),
+        #Log to console
+        log_response=True
+    )
+
+    #Now we want to access this data
+    # airlfow automatically stores return data into its xcoms
+
+
+    #If you go to admin>xcom return values it will be the json objects
+
+   #This task will save the return value locally
+    task_save = PythonOperator(task_id='save_grades',
+        python_callable=save_students
+    )
+
     TA_Hire = PythonOperator(task_id='hire',
                          python_callable=hire)
 
     Add_Projects = PythonOperator(task_id='add',
                          python_callable=add)
-                     
+
     Enroll_Student = PythonOperator(task_id='enroll',
                          python_callable=enroll)
 
-                         
+
     Assign_Student = PythonOperator(task_id='assign',
                          python_callable=assignStudents)
 
     Student_Questionnaire = PythonOperator(task_id='student_Q',
                             python_callable=_student_questionnaire)
-    
-    Assign_Leader = PythonOperator(task_id='assignLead',
+
+    Assign_Leader = PythonOperator(task_id='assign_TeamLead',
                             python_callable=assignLead)
 
-        
+
     Assign_TA = PythonOperator(task_id='assignTA',
                             python_callable=assignTAs)
 
     Planning_Phase = PythonOperator(task_id='planning',
                             python_callable=_planning_phase)
-    
+
     Quizzes = PythonOperator(task_id='quizzes',
                             python_callable=quizzes)
-    
-    Iterations = PythonOperator(task_id='_iterations',
-                            python_callable=_iterations)
+
+    Retro = PythonOperator(task_id='it1_retrospective',
+                            python_callable=quizzes)
+
+    Retro2 = PythonOperator(task_id='it2_retrospective',
+                            python_callable=quizzes)
+
+    Retro3 = PythonOperator(task_id='it3_retrospective',
+                            python_callable=quizzes)
+
+    Retro4 = PythonOperator(task_id='it4_retrospective',
+                            python_callable=quizzes)
+
+    Retro0 = PythonOperator(task_id='it0_retrospective',
+                            python_callable=quizzes)
+
+    Iteration0 = [
+        PythonOperator(
+            task_id=f"it0{model_id}",
+            python_callable=_iterations,
+            op_kwargs={
+            "model": model_id
+            }
+        ) for model_id in ['_Planning_Meeting', '_Standup', '_Review_Meeting']
+    ]
+
+    Iteration1 = [
+        PythonOperator(
+            task_id=f"it1{model_id}",
+            python_callable=_iterations,
+            op_kwargs={
+            "model": model_id
+            }
+        ) for model_id in ['_Planning_Meeting', '_Standup', '_Review_Meeting']
+    ]
+
+
+
+    Iteration2 = [
+        PythonOperator(
+            task_id=f"it2{model_id}",
+            python_callable=_iterations,
+            op_kwargs={
+            "model": model_id
+            }
+        ) for model_id in ['_Planning_Meeting', '_Standup', '_Review_Meeting']
+    ]
+
+
+    Iteration3 = [
+        PythonOperator(
+            task_id=f"it3{model_id}",
+            python_callable=_iterations,
+            op_kwargs={
+            "model": model_id
+            }
+        ) for model_id in ['_Planning_Meeting', '_Standup', '_Review_Meeting']
+    ]
+
+    Iteration4 = [
+        PythonOperator(
+            task_id=f"it4{model_id}",
+            python_callable=_iterations,
+            op_kwargs={
+            "model": model_id
+            }
+        ) for model_id in ['_Planning_Meeting', '_Standup', '_Review_Meeting']
+    ]
+
 
     Closing_Document = PythonOperator(task_id='closing_document',
                             python_callable=closing)
@@ -136,6 +246,8 @@ catchup=False) as dag:
 
 
 
-#Dependencies 
-TA_Hire >> Add_Projects >> Enroll_Student >> Student_Questionnaire >> Assign_Student >> Assign_Leader >> Assign_TA >> training_model_tasks >> Planning_Phase >> Quizzes >> Iterations >> Closing_Document >> final_grades >> [_pass,_fail]
+
+
+#Dependencies
+TA_Hire >> Add_Projects >> Enroll_Student >> Student_Questionnaire >> Assign_Student >> Assign_Leader >> Assign_TA >> training_model_tasks >> Planning_Phase >> Quizzes >> Iteration0 >> Retro0 >> Iteration1 >> Retro >> Iteration2 >> Retro2 >> Iteration3 >> Retro3 >> Iteration4 >> Retro4 >> Closing_Document >>   task_is_api_active >> task_get_users >> task_save >> final_grades >> [_pass,_fail]
 
